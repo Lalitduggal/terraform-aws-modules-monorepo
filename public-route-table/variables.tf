6071cb985eeaff892ef9e9c913355335e09e594f
lalit duggal <Lalit.duggal@gmail.com>
2021-09-19 16:20:17 +1000

    Update variables.tf

--- public-route-table/variables.tf
+++ public-route-table/variables.tf
@@ -1,11 +1,11 @@
-variable "my_public_route_table_name" {
+variable "my_public_subnet_route_table_name" {
     type = string
     default = "testing-pub-rt"
-    description = "Name of the public route table"
+    description = "Name of the public subnet route table"
 }
 
 
-variable "my_public_route_table_vpc_id" {
+variable "my_public_subnet_route_table_vpc_id" {
     type = string
     description = "vpc id where the public route table"
 }
