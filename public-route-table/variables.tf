c7716a7a09632399c709d9f3ae725e44137c5bb6
lalit duggal <Lalit.duggal@gmail.com>
2021-09-19 13:47:00 +1000

    Update variables.tf

--- public-route-table/variables.tf
+++ public-route-table/variables.tf
@@ -1,1 +1,11 @@
+variable "my_public_route_table_name" {
+    type = string
+    default = "testing-pub-rt"
+    description = "Name of the public route table"
+}
 
+
+variable "my_public_route_table_vpc_id" {
+    type = string
+    description = "vpc id where the public route table"
+}
