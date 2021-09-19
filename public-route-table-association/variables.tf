965e53a47e079b8bbdc1b6e94f51cf36d6e89d2e
lalit duggal <Lalit.duggal@gmail.com>
2021-09-19 14:00:34 +1000

    Update variables.tf

--- public-route-table-association/variables.tf
+++ public-route-table-association/variables.tf
@@ -1,1 +1,11 @@
 
+variable "my_public_route_table_id" {
+    type = string
+    description = "Id of the public route table"
+}
+
+
+variable "my_public_subnet_id" {
+    type = string
+    description = "Id of the public subnet"
+}
