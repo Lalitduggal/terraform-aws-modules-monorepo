c7c1126b0a0b009e39c9e60b47981c67959fa28d
lalit duggal <Lalit.duggal@gmail.com>
2021-09-19 17:34:34 +1000

    Update main.tf

--- route/main.tf
+++ route/main.tf
@@ -1,5 +1,6 @@
 resource "aws_route" "route_for_igw" {
   route_table_id            = var.public_subnet_route_table_id
+  destination_cidr_block = var.route_for_igw_destination_cidr_block
   gateway_id = var.igw_id
   #destination_cidr_block    = var.destination_cidr_block
   #vpc_peering_connection_id = var.vpc_peering_connection_id
