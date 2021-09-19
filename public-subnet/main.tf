7bf781963fe95c3fe0fd03fc3152cd4032b8a575
lalit duggal <Lalit.duggal@gmail.com>
2021-09-19 13:22:13 +1000

    Update main.tf

--- public-subnet/main.tf
+++ public-subnet/main.tf
@@ -1,8 +1,8 @@
 resource "aws_subnet" "my_public_subnet" {
   vpc_id     = var.my_public_subnet_vpc_id
-  cidr_block = var.public_subnet_cidr_block
+  cidr_block = var.my_public_subnet_cidr_block
 
   tags = {
-    Name = var.public_subnet_name
+    Name = var.my_public_subnet_name
   }
 }
