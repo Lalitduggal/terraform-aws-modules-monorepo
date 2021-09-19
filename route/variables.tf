1c2ed3216e16aee7722c99a275e4939fd96fefd8
lalit duggal <Lalit.duggal@gmail.com>
2021-09-19 16:59:55 +1000

    Update variables.tf

--- route/variables.tf
+++ route/variables.tf
@@ -1,1 +1,12 @@
+variable "public_subnet_route_table_id" {
+    type = string
+    default = "public-subnet-rt-id"
+    description = "ID of public subnet route table"
+}
 
+
+variable "igw_id" {
+    type = string
+    default = "igw-id"
+    description = "igw id"
+}
