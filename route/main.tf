55ca1513b97e557d07b0c15658fc7e8cccbb40bc
lalit duggal <Lalit.duggal@gmail.com>
2021-09-19 16:57:09 +1000

    Update main.tf

--- route/main.tf
+++ route/main.tf
@@ -1,6 +1,7 @@
 resource "aws_route" "route_for_igw" {
-  route_table_id            = "rtb-4fbb3ac4"
-  destination_cidr_block    = "10.0.1.0/22"
-  vpc_peering_connection_id = "pcx-45ff3dc1"
+  route_table_id            = var.public_subnet_route_table_id
+  gateway_id = var.igw_id
+  #destination_cidr_block    = var.destination_cidr_block
+  #vpc_peering_connection_id = var.vpc_peering_connection_id
   #depends_on                = [aws_route_table.testing]
 }
