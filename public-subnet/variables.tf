3116aa1a4584aa8c15e668770326d3ad4d867838
lalit duggal <Lalit.duggal@gmail.com>
2021-09-19 13:05:45 +1000

    Update variables.tf

--- public-subnet/variables.tf
+++ public-subnet/variables.tf
@@ -1,1 +1,11 @@
+variable "my_public_subnet_name" {
+    type = string
+    default = "testing-public-subnet"
+    description = "Name of the public subnet"
+}
 
+
+variable "my_public_subnet_vpc_id" {
+    type = string
+    description = "vpc id where the public subnet will be created"
+}
