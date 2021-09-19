67f164c5934d1c89818b338366c3f318ad636453
lalit duggal <Lalit.duggal@gmail.com>
2021-09-19 11:06:49 +1000

    Update main.tf

--- igw/main.tf
+++ igw/main.tf
@@ -1,10 +1,3 @@
 resource "aws_internet_gateway" "my_igw" {
-  vpc_id = output "my_vpc_id" {
-  value = my_vpc.id
-  description = "This is the vpc id"
-}
-
-  tags = {
-    Name = var.my_igw_name
-  }
-}
+  vpc_id = 
+  
