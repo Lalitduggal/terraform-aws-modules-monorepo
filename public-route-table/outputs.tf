6f828d30969b202d112fd1626393e1395ed9703f
lalit duggal <Lalit.duggal@gmail.com>
2021-09-19 16:19:13 +1000

    Update outputs.tf

--- public-route-table/outputs.tf
+++ public-route-table/outputs.tf
@@ -1,4 +1,4 @@
 output "my_public_subnet_route_table_id" {
-  value = aws_subnet.my_public_subnet_route_table.id
+  value = aws_route_table.my_public_subnet_route_table.id
   description = "This is the public subnet route table id"
 }
