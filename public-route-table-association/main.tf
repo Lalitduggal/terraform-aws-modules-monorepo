7b01ea2772e3a7126c0a9c661f8036720416f3e9
lalit duggal <Lalit.duggal@gmail.com>
2021-09-19 13:57:32 +1000

    Update main.tf

--- public-route-table-association/main.tf
+++ public-route-table-association/main.tf
@@ -1,4 +1,4 @@
 resource "aws_route_table_association" "my_public_route_table_association" {
-  subnet_id      = aws_subnet.foo.id
-  route_table_id = aws_route_table.bar.id
+  subnet_id      = var.my_public_subnet_id
+  route_table_id = var.my_public_subnet_route_table_id
 }
