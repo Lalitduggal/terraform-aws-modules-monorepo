a07834132502ed7629e56518cf378af0c1fd441c
lalit duggal <Lalit.duggal@gmail.com>
2021-09-19 13:03:27 +1000

    Update main.tf

--- public-subnet/main.tf
+++ public-subnet/main.tf
@@ -1,5 +1,5 @@
-resource "aws_subnet" "main" {
-  vpc_id     = aws_vpc.main.id
+resource "aws_subnet" "my_public_subnet" {
+  vpc_id     = var.my_public_subnet_vpc_id
   cidr_block = var.public_subnet_cidr_block
 
   tags = {
